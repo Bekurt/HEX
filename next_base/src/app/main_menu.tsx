b48a9e0c914d1d1ad7d8b85a188072159d4e9f90
Bekurt <attilio.dinunno@gmail.com>
2023-09-27 13:17:53 +0200

    Added background and main screen
    Plus some dependancies

---
+++ next_base/src/app/main_menu.tsx
@@ -0,0 +1,15 @@
+export function MainMenu() {
+  return (
+    <div
+      id="background"
+      className="w-full h-full flex items-center bg-hexTri bg-[length:1400px_840px] bg-center animate-marqee overflow-hidden"
+    >
+      <div
+        id="menu-bar"
+        className="w-full h-1/3 py-2 border border-black bg-sky-500"
+      >
+        {/* <TitleScreen /> */}
+      </div>
+    </div>
+  );
+}
--- next_base/tailwind.config.ts
+++ next_base/tailwind.config.ts
@@ -1,20 +1,38 @@
-import type { Config } from 'tailwindcss'
+import { url } from "inspector";
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        hexTri: "url('./img/HexTrimmed.png')",
+        hexHero: "url('./img/hexagons.svg')",
+      },
+      animation: {
+        marqee: "marquee 30s linear 0s infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": {
+            "background-position-x": "0px",
+            "background-position-y": "0px",
+          },
+          "100%": {
+            "background-position-x": "1400px",
+            "background-position-y": "-840px",
+          },
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
