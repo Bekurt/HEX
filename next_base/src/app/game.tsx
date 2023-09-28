edc9aef85daa695105bc01967b079b9ddf9e4313
Bekurt <attilio.dinunno@gmail.com>
2023-09-28 11:28:42 +0200

    The board now resizes with the screen

--- next_base/src/app/game.tsx
+++ next_base/src/app/game.tsx
@@ -1,19 +1,26 @@
 import { useEffect, useRef, useState } from "react";
 
 export function GameInterface() {
+  const dimRef = useRef(null);
+
   return (
     <main id="game" className="w-full h-full flex">
-      <section id="game-side" className="h-full w-9/12">
-        <header id="hint" className="bg-sky-700 text-black text-sm h-9 p-2">
+      <section id="game-side" className="h-full w-9/12 flex flex-col">
+        <header
+          id="hint"
+          className="bg-sky-700 text-black p-2 lg:p-2.5 xl:p-3 2xl:p-3.5 text-sm lg:text-base xl:text-lg 2xl:text-xl h-[10%] max-h-16 flex-shrink-0"
+        >
           Try to connect the opposite sides of the board that match your color!
           First player is Blue
         </header>
-        <GameBoard size={14} />
+        <div id="boundary" ref={dimRef} className="w-full h-[90%] flex-grow">
+          <GameBoard size={14} dimRef={dimRef} />
+        </div>
       </section>
       <section id="move-history" className="h-full w-3/12 bg-slate-500">
         <header
           id="history-title"
-          className="bg-slate-600 text-black text-sm h-9 p-2"
+          className="bg-slate-600 text-black p-2 lg:p-2.5 xl:p-3 2xl:p-3.5 text-sm lg:text-base xl:text-lg 2xl:text-xl h-[10%] max-h-16"
         >
           Move history
         </header>
@@ -29,7 +36,12 @@ interface Tiles {
   owner: string;
 }
 
-function GameBoard({ size = 6 }) {
+interface Box {
+  width: number;
+  height: number;
+}
+
+function GameBoard({ size = 6, dimRef }: { size: number; dimRef: any }) {
   //Setup of empty board
   const emptyBoard: Tiles[] = [];
   for (let i = 0; i < size * size; i++) {
@@ -43,33 +55,32 @@ function GameBoard({ size = 6 }) {
 
   //Hooks
   const [boardState, setBoardState] = useState(emptyBoard);
-  const [criticalDimension, setCriticalDimension] = useState(300);
-  const dimRef = useRef(null);
+  const [boxSize, setboxSize] = useState({ width: 0, height: 0 });
 
   // Create the board
-  const board = createBoard(boardState, criticalDimension);
+  const board = createBoard(boardState, boxSize);
 
-  // useEffect(() => {
-  //   let observer = new ResizeObserver((entries, observer) => {
-  //     const { inlineSize: width, blockSize: height } =
-  //       entries[0].contentBoxSize[0];
-  //     setCriticalDimension(height);
+  useEffect(() => {
+    let observer = new ResizeObserver((entries) => {
+      const { inlineSize: width, blockSize: height } =
+        entries[0].contentBoxSize[0];
+      setboxSize({ width, height });
 
-  //     observer.disconnect();
-  //   });
+      //observer.disconnect();
+    });
 
-  //   observer.observe(dimRef as unknown as Element);
-  //   return () => {
-  //     observer.disconnect();
-  //   };
-  // }, []);
+    observer.observe(dimRef.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, [boxSize, dimRef]);
 
   return (
     <svg
       id="game-board"
-      viewBox={`0 ${
-        -0.2 * criticalDimension
-      } ${criticalDimension} ${criticalDimension}`}
+      viewBox={`${-0.1 * boxSize.width} ${-0.1 * boxSize.height} ${
+        boxSize.width
+      } ${boxSize.height}`}
       className="h-full w-full"
     >
       {board}
@@ -77,11 +88,16 @@ function GameBoard({ size = 6 }) {
   );
 }
 
-function createBoard(boardState: Tiles[], height: number): JSX.Element {
+// Returns
+function createBoard(boardState: Tiles[], box: Box): JSX.Element {
   const rowNum = Math.sqrt(boardState.length);
   const RAD3 = Math.sqrt(3);
-  const boardUnit = rowNum * RAD3 + (RAD3 / 2) * (rowNum - 1);
-  const sideLength = Math.round((0.8 * height) / boardUnit);
+  const boardUnitWidth = rowNum * RAD3 + (RAD3 / 2) * (rowNum - 1);
+  const boardUnitHeight = rowNum * 1.5 + 2;
+  const sideLength = Math.min(
+    Math.round((0.8 * box.height) / boardUnitHeight),
+    Math.round((0.8 * box.width) / boardUnitWidth)
+  );
 
   let JSX = boardState.map((e) => {
     let pointString = `${(e.col * RAD3 + (e.row * RAD3) / 2) * sideLength},${
@@ -115,6 +131,7 @@ function createBoard(boardState: Tiles[], height: number): JSX.Element {
   return <>{JSX}</>;
 }
 
+// Returns the right class based on the player who owns the tile
 function translateOwner(owner: string): string {
   switch (owner) {
     case "blue":
